8c58d826301bc9a7f247b303a937767df9d0153b
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-08-22 20:14:32 +0530

    Added App Columns

--- desktop_portfolio/src/App.js
+++ desktop_portfolio/src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div class="flex bg-black min-h-screen">
+      <div class="fixed left-0 text-white bg-slate-800 min-h-full w-60">
+        <div class="absolute left-0 px-5">COLUMN 1</div>
+        <div class="absolute right-0 px-5">COLUMN 2</div>
+      </div>
       <div class="bg-blue-400 fixed bottom-0 left-0 right-0 h-10">
         <div class="bg-slate-50 px-1 py-0.5 absolute my-2 mx-2 left-0">
           Start
