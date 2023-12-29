ad7f61de65707940448d1f2f2f2eb0ab2d2d4506
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-12-29 15:16:21 +0530

    Start Menu Ready

--- desktop_portfolio/src/assets/startMenu.js
+++ desktop_portfolio/src/assets/startMenu.js
@@ -1,20 +1,94 @@
 import React from "react";
+import StartMenuLogo from "./win95Start.png";
+import Instagram from "./instagram.png";
+import Twitter from "./twitter.png";
+import LinkedIn from "./linkedin.png";
+import GitHub from "./github-sign.png";
+import Resume from "./folder.png";
+import Portfolio from "./briefcase.webp";
+import Facebook from "./facebook.png";
 const StartMenu = () => {
-  const items = ["Hi", "Hello","Hi", "Hello","Hi", "Hello"];
+  class StartItem {
+    constructor(name, logo, link) {
+      this.name = name;
+      this.logo = logo;
+      this.link = link;
+    }
+  }
+  const linkedin = new StartItem(
+    "LinkedIn",
+    LinkedIn,
+    "https://www.linkedin.com/in/heysubinoy/"
+  );
+  const instagram = new StartItem(
+    "Instagram",
+    Instagram,
+    "https://www.instagram.com/biswas.subinoy"
+  );
+  const twitter = new StartItem(
+    "Twitter",
+    Twitter,
+    "https://twitter.com/heysubinoy"
+  );
+  const github = new StartItem(
+    "GitHub",
+    GitHub,
+    "https://github.com/subinoybiswas"
+  );
+  const resume = new StartItem(
+    "Resume",
+    Resume,
+    "https://drive.google.com/file/d/1p3MEOQUIvwDG5H6QbCb-UTtoWyQ9CnEh/view?usp=sharing"
+  );
+  const portfolio = new StartItem("Portfolio", Portfolio, "https://subinoy.me");
+  const facebook = new StartItem(
+    "Facebook",
+    Facebook,
+    "https://www.facebook.com/biswas.subinoy/"
+  );
+
+  const items = [
+    linkedin,
+    instagram,
+    twitter,
+    github,
+    facebook,
+    resume,
+    portfolio,
+  ];
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
-    <div class=" absolute h-[30rem] w-[20rem] bg-white left-0 bottom-10 z-[100]">
-      <div class="grid grid-flow-col ">
-        <div class=" bg-emerald-300">
-          {/* <div class="  fixed text-black bg-zinc-50 w-[200px] h-[35px] ml-1 -left-[90px] bottom-[19%] -rotate-90">
-    Windows Me
-  </div> */}
+    <div class=" absolute h-[17.5rem] w-[12rem] border-t-2 border-b-2 border-l-2 border-r-2 border-s-2 bg-white left-0 bottom-10 z-[100]">
+      <div class="grid grid-cols-6 ">
+        <div className="relative bg-[#7b7d7b]">
+          <img
+            src={StartMenuLogo}
+            alt="StartMenuLogo"
+            className="absolute bottom-0"
+          />
         </div>
+
         <div class="bg-black col-span-5 grid grid-flow-row auto-rows-max">
           {/* <div class="bg-blue-400 row-[span_16_/_span_16]">Hi</div> */}
           {items.map((item) => (
             // Conditionally render based on the value of the 'on' prop
-            <div class="bg-blue-400 h-12 border-2 text-center py-1 border-slate-50">
-              {item}
+
+            <div
+              className="grid grid-cols-3 bg-[#c3c3c3] hover:bg-[#00007c] h-10 "
+              onClick={() => openInNewTab(item.link)}
+            >
+              <img
+                src={item.logo}
+                alt={item.name}
+                class="w-[30px] place-self-center"
+              ></img>
+
+              <div class=" col-span-2 py-1 text-[17px] font-black">
+                {item.name}
+              </div>
             </div>
           ))}
         </div>
